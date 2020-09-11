6b701a6f34e854aa2a860317ab55dc8e36c1154b
K-DH <40789695+K-DH@users.noreply.github.com>
2020-09-11 15:19:53 +0900

    Update 나이를 입력받고 출력하는 코드 추가

--- firstExam.c
+++ firstExam.c
@@ -5,7 +5,11 @@
 #include <stdio.h>
 
 int main(int argc , char* argv[] ) {
-
-   printf("hello.c\n");
+   
+   int age = 0;
+    
+   printf("나이를 입력하세요");
+   scanf("%d" , &age);
+   printf("age : %d" , age);
    return 0;
 }
