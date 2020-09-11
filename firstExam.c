3ae12b5bb1dad75a0cae632a88db052b6a725a03
K-DH <40789695+K-DH@users.noreply.github.com>
2020-09-11 15:21:11 +0900

    Update 이름을 입력받고 출력하는 명령 추가

--- firstExam.c
+++ firstExam.c
@@ -7,9 +7,14 @@
 int main(int argc , char* argv[] ) {
    
    int age = 0;
-    
+   char name[50];
+  
+   printf("이름을 입력하세요");
+   scanf("%s",name);
    printf("나이를 입력하세요");
    scanf("%d" , &age);
+  
+   printf("name : %d" , name);
    printf("age : %d" , age);
    return 0;
 }
